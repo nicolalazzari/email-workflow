0f0de7530cac9430f597597e62f002b57bab7097
Nicola Lazzari <nicolalazzari@gmail.com>
2015-10-27 17:23:22 +0000

    NewCTA-CSSfiles
    still need to fix few responsive issues (CTA / Hero)

--- src/helpers/helper-footer.js
+++ src/helpers/helper-footer.js
@@ -1,33 +1,22 @@
-// 
+//
 // Helper for Footer
 //
 module.exports.footer = function (params) {
-  return '\
-<table class="backgroundTable" align="center">\
+return '\
+<table class="backgroundTable" align="center" cellpadding="0" cellspacing="0" border="0">\
     <tbody>\
         <tr>\
             <td align="center">\
                 <table cellpadding="0" cellspacing="0" border="0" align="center" class="container" >\
                     <tbody>\
                         <tr>\
-                            <td width="100%">\
-                                <table cellpadding="0" cellspacing="0" border="0" align="center" class="container" >\
-                                    <tbody>\
-                                        <tr>\
-                                            <td width="100%" height="20"></td>\
-                                        </tr>\
-                                        <tr>\
-                                            <td align="center" valign="middle" style="font-family: Helvetica, arial, sans-serif; font-size: 14px;">Copyright ' + params.hash.year + ' | ' + params.hash.brand + ' is a Division of MVF</td>\
-                                        </tr>\
-                                        <tr>\
-                                            <td width="100%" height="20"></td>\
-                                        </tr>\
-                                    </tbody>\
-                                </table>\
-                            </td>\
+                            <td width="100%" height="20"></td>\
                         </tr>\
                         <tr>\
-                            <td  height="15" style="font-size:1px; line-height:1px; mso-line-height-rule: exactly;">&nbsp;</td>\
+                            <td align="center" valign="middle"><h4>Copyright ' + params.hash.year + ' | ' + params.hash.brand + ' is a Division of MVF</h4></td>\
+                        </tr>\
+                        <tr>\
+                            <td width="100%" height="20"></td>\
                         </tr>\
                     </tbody>\
                 </table>\
@@ -35,5 +24,5 @@ module.exports.footer = function (params) {
         </tr>\
     </tbody>\
 </table>\
-  '
+'
 };
