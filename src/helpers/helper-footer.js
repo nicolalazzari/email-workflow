b770ae8a84aa7f6df7c683d0097f691f9c761a9c
Nicola Lazzari <nicolalazzari@gmail.com>
2015-11-10 14:42:13 +0000

    tested-hbs-sass

--- src/helpers/helper-footer.js
+++ src/helpers/helper-footer.js
@@ -3,26 +3,6 @@
 //
 module.exports.footer = function (params) {
 return '\
-<table class="backgroundTable" align="center" cellpadding="0" cellspacing="0" border="0">\
-    <tbody>\
-        <tr>\
-            <td align="center">\
-                <table cellpadding="0" cellspacing="0" border="0" align="center" class="container" >\
-                    <tbody>\
-                        <tr>\
-                            <td width="100%" height="20"></td>\
-                        </tr>\
-                        <tr>\
-                            <td align="center" valign="middle"><h4>Copyright ' + params.hash.year + ' | ' + params.hash.brand + ' is a Division of MVF</h4></td>\
-                        </tr>\
-                        <tr>\
-                            <td width="100%" height="20"></td>\
-                        </tr>\
-                    </tbody>\
-                </table>\
-            </td>\
-        </tr>\
-    </tbody>\
-</table>\
+<h4>Copyright ' + params.hash.year + ' | ' + params.hash.brand + ' is a Division of MVF</h4>\
 '
 };
